7d688ff07ea7aa99fa17fa15160cf8ef8ec0f1f9
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-14 15:53:58 +0430

    Add Update Classes

---
+++ app/src/main/java/com/example/tmdbclient/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.tmdbclient.util
+
+object Constants {
+
+    const val API_KEY = "e0ab34c7a4d43a3bbb56b8d22d27e247"
+
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+
+    const val LANGUAGE = "en-US"
+
+}
