5ca48bd1a4eb0293f92f003af665ddfb54c70e6c
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-14 13:32:40 +0430

    Add Popular Movie Dto

---
+++ app/src/main/java/com/example/tmdbclient/data/model/popula_rmovie_dto/PopularMoviesResponse.kt
@@ -0,0 +1,15 @@
+package com.example.tmdbclient.data.model.popula_rmovie_dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class PopularMoviesResponse(
+    @SerializedName("page")
+    val page: Int?,
+    @SerializedName("results")
+    val popularMovies: List<PopularMovie?>?,
+    @SerializedName("total_pages")
+    val totalPages: Int?,
+    @SerializedName("total_results")
+    val totalResults: Int?
+)
