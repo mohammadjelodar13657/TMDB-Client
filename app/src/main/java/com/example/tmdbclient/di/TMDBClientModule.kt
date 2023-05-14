0c9d53972c7be6f0e14b9e9933743d708a2e1fc3
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-14 16:37:43 +0430

    Update TMDBModule For Retrofit and TMDBApi and Interceptor

--- app/src/main/java/com/example/tmdbclient/di/TMDBClientModule.kt
+++ app/src/main/java/com/example/tmdbclient/di/TMDBClientModule.kt
@@ -1,10 +1,53 @@
 package com.example.tmdbclient.di
 
+import com.example.tmdbclient.data.remote.TMDBApi
+import com.example.tmdbclient.util.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class TMDBClientModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpInterceptor(): Interceptor = Interceptor { chain ->
+        val url = chain.request()
+            .url
+            .newBuilder()
+            .addQueryParameter("api_key", Constants.API_KEY)
+            .addQueryParameter("language", Constants.LANGUAGE)
+            .build()
+        val request = chain.request()
+            .newBuilder()
+            .url(url)
+            .build()
+        chain.proceed(request)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(interceptor: Interceptor): OkHttpClient =
+        OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideTMDBApi(retrofit: Retrofit): TMDBApi =
+        retrofit.create(TMDBApi::class.java)
 }
