6eeac0115b9b674cdd21c12849c07d2c21395641
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-15 14:32:01 +0430

    Remove @AndroidEntryPoint Annotation

--- app/src/main/java/com/example/tmdbclient/data/repository/remote/popularmovie/PopularMovieRepository.kt
+++ app/src/main/java/com/example/tmdbclient/data/repository/remote/popularmovie/PopularMovieRepository.kt
@@ -3,7 +3,6 @@ package com.example.tmdbclient.data.repository.remote.popularmovie
 import com.example.tmdbclient.data.remote.TMDBApi
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class PopularMovieRepository(private val tmdbApi: TMDBApi) {
 
     suspend fun getPopularMovies(page: Int) = tmdbApi.getPopularMovies(page)
