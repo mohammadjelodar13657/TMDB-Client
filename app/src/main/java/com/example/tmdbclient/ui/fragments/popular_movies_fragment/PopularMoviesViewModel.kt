4dabd632eee81174f42ac1af336c1cf27425ffac
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-15 07:27:48 +0430

    Add Popular Movies ViewModel

---
+++ app/src/main/java/com/example/tmdbclient/ui/fragments/popular_movies_fragment/PopularMoviesViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.tmdbclient.ui.fragments.popular_movies_fragment
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tmdbclient.data.model.popula_rmovie_dto.PopularMovie
+import com.example.tmdbclient.data.repository.remote.popularmovie.PopularMovieRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class PopularMoviesViewModel(private val popularMovieRepository: PopularMovieRepository): ViewModel() {
+
+    private val _popularMoviesList = MutableLiveData<List<PopularMovie?>?>()
+    val popularMoviesList: LiveData<List<PopularMovie?>?> = _popularMoviesList
+
+    fun getPopularMovies() {
+        viewModelScope.launch(IO) {
+            _popularMoviesList.value =  popularMovieRepository.getPopularMovies(1).popularMovies
+        }
+    }
+}
