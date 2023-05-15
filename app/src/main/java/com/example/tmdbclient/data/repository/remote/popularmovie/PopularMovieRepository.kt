4a3a67f2963894bd184ed0fabbb1edf8e37da131
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-15 06:34:32 +0430

    Update Popular Movies Fragment With @AndroidEntryPoint Annotation

--- app/src/main/java/com/example/tmdbclient/data/repository/remote/popularmovie/PopularMovieRepository.kt
+++ app/src/main/java/com/example/tmdbclient/data/repository/remote/popularmovie/PopularMovieRepository.kt
@@ -1,7 +1,9 @@
 package com.example.tmdbclient.data.repository.remote.popularmovie
 
 import com.example.tmdbclient.data.remote.TMDBApi
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PopularMovieRepository(private val tmdbApi: TMDBApi) {
 
     suspend fun getPopularMovies(page: Int) = tmdbApi.getPopularMovies(page)
